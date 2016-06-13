53f5b18c734818584aecb537fba40a1ae82d1f75
bestaone <bestaone@163.com>
2016-06-13 23:10:06 +0800

    yunqi

---
+++ apis/user/service/src/main/java/com/yunqi/apis/user/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.yunqi.apis.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CommonController {
+	
+//	@RequestMapping("error")
+//	public Exception error() {
+//		return new Exception("error");
+//	}
+
+}
