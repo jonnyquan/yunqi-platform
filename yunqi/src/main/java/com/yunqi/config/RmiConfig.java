2b496f154955b7feb3a5b2c5ba9892cab0885f20
bestaone@163.com <bestaone@163.com>
2016-06-17 18:15:17 +0800

    yunqi

--- yunqi/src/main/java/com/yunqi/config/RmiConfig.java
+++ yunqi/src/main/java/com/yunqi/config/RmiConfig.java
@@ -11,16 +11,6 @@ import com.yunqi.apis.user.api.UserApi;
 @Configuration
 public class RmiConfig {
 
-//    @Bean(name="userApi")
-//    public UserApi userApi(){  
-//    	HttpServiceExporter exporter = new HttpServiceExporter();  
-//        exporter.setServiceInterface(UserApi.class);  
-//        exporter.setServiceUrl("http://127.0.0.1:8080/user"); 
-//        UserApi userApi = (UserApi) exporter.getService();
-//        return userApi;
-//    }  
-	
-	
 	@Bean(name = "userApi")
 	public UserApi userApi() {
 		UserApi api = null;
