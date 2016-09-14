2910b424d95c69c22bad7ca2642929bc1727d332
bestaone@163.com <bestaone@163.com>
2016-09-14 14:43:29 +0800

    spring boot 升级 1.3.5.RELEASE 到 1.4.0.RELEASE

--- yunqi/src/main/java/com/yunqi/web/Application.java
+++ yunqi/src/main/java/com/yunqi/web/Application.java
@@ -3,7 +3,7 @@ package com.yunqi.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.yunqi.web"})
 public class Application extends SpringBootServletInitializer {
