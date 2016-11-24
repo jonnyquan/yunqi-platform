afaabe0e03e0593b8f25851ecfe06f1dcf7d0309
bestaone@163.com <bestaone@163.com>
2016-11-24 16:56:04 +0800

    添加redis支持的异步log,设置日志channl

---
+++ commons/logger/src/main/java/com/yunqi/logger/RedisAppender.java
@@ -0,0 +1,37 @@
+package com.yunqi.logger;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Layout;
+
+public class RedisAppender extends ch.qos.logback.core.AppenderBase<ILoggingEvent> {
+
+	private RedisPusher pusher;
+
+	private Layout<ILoggingEvent> layout;
+
+	@Override
+	public void start() {
+		super.start();
+		pusher.init();
+	}
+
+	public void setPusher(RedisPusher pusher) {
+		this.pusher = pusher;
+	}
+
+	@Override
+	protected void append(ILoggingEvent eventObject) {
+		try {
+			StringBuilder builder = new StringBuilder();
+			builder.append(layout.doLayout(eventObject));
+			pusher.push(builder.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setLayout(Layout<ILoggingEvent> layout) {
+		this.layout = layout;
+	}
+
+}
