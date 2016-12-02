4264fd734a9a52d9096976caff51a6f2a92ad5db
bestaone@163.com <bestaone@163.com>
2016-12-02 14:02:40 +0800

    修改日志

--- commons/logger/src/main/java/com/yunqi/logger/RedisPusher.java
+++ commons/logger/src/main/java/com/yunqi/logger/RedisPusher.java
@@ -1,21 +1,40 @@
 package com.yunqi.logger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by bestaone on 2016/11/24.
  */
 public class RedisPusher {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private String host;
     private int port;
     private String channel;
     private String auth;
     private Jedis jedis;
+    private Pipeline pipeline;
+    private static int count = 0;
 
     public void init(){
         jedis = new Jedis(host, port);
         jedis.auth(auth);
+        pipeline = jedis.pipelined();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                submit();
+            }
+        }, 10000, 5000); //延时10秒，每5秒执行一次
     }
 
     public void push(String type, String msg){
@@ -23,17 +42,26 @@ public class RedisPusher {
         if (null == msg) return;
 
         if(null == channel){
-            System.out.println("LOGGER WARRING: NO CHANNEL GENERATED.");
+            logger.debug("LOGGER WARRING: NO CHANNEL GENERATED.");
             return;
         }
 
         if(null == jedis){
-            System.out.println("LOGGER WARRING: NO REDIS GENERATED.");
+            logger.debug("LOGGER WARRING: NO REDIS GENERATED.");
             return;
         }
 
-        jedis.publish(channel + "." + type, msg);
+        pipeline.publish(channel + "." + type, msg);
 
+        if(++count>=5){
+            submit();
+        }
+
+    }
+
+    private void submit(){
+        pipeline.sync();
+        count = 0;
     }
 
     public void close() {
