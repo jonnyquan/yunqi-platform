223b8440cac5585fe04cf85c73eaf4ea276f80ef
bestaone@163.com <bestaone@163.com>
2016-12-22 16:23:11 +0800

    调整asyncall

--- apis/org/service/src/main/java/com/yunqi/apis/org/config/AsynApiConfig.java
+++ apis/org/service/src/main/java/com/yunqi/apis/org/config/AsynApiConfig.java
@@ -1,17 +1,10 @@
 package com.yunqi.apis.org.config;
 
 import com.earven.asyncall.AsyncallConfig;
-import com.earven.asyncall.client.QueueProvider;
-import com.earven.asyncall.client.RedisTemplateQueueProvider;
-import com.yunqi.asyn.api.org.OrgTestAsynApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.earven.asyncall.service.Module;
-import redis.clients.jedis.JedisPool;
 
 @Configuration
 public class AsynApiConfig extends AsyncallConfig implements ApplicationContextAware {
